4fa32ce15e36fb687036cc74eb4b1b3558e6d010
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-01-19 18:48:08 +0530

    done

--- script.js
+++ script.js
@@ -31,9 +31,3 @@ setTimeout(function(){
     document.querySelector("#loader").style.top = "-100%";
 }, 4000)
 
-var fullscreen = document.querySelector("#full-screen");
-var menu = document.querySelector("#menu")
-menu.addEventListener("click", function(){
-    fullscreen.style.top = "0%";
-    // document.querySelector("#nav img").style.opacity = 0;
-})
